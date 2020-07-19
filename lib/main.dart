83d6284e8e19fcb363be2ecc855963a5fb8f5af3
Tbrahn <tbranham9323@gmail.com>
2020-07-18 22:33:15 -0400

    add styling to price card

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,26 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(
+                        10,
+                      ),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
                     ),
                     Column(
